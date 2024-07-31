2948915d4213d653a9cd2a22df9fd0072213c6d7
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-07-30 23:54:22 -0300

    Cambio nomenclatura a {name}.env

--- izienv/_load_izienv.py
+++ izienv/_load_izienv.py
@@ -1,19 +1,32 @@
 from pathlib import Path
 from dotenv import load_dotenv
 
-def __path_env(path_envs: Path, name: str) -> Path:
-    return path_envs / f".env_{name}"
+def get_path_env(path_envs: Path, name: str) -> Path:
+    return path_envs / f"{name}.env"
 
 def __load_env_base(path_envs: Path, override: bool = True) -> None:
-    path_base = __path_env(path_envs=path_envs, name="base")
+    path_base = get_path_env(path_envs=path_envs, name="base")
     if path_base.exists():
         load_dotenv(path_base, override=override)
+    else:
+        print(f"base.env dont exists into {path_envs}")
+
+def __load_env_etc(path_envs: Path, override: bool = True) -> None:
+    path_etc = get_path_env(path_envs=path_envs, name="etc")
+    if path_etc.exists():
+        load_dotenv(path_etc, override=override)
 
 
-def load_izienv(path_envs: Path, name: str, override: bool = True) -> None:
-    # Load .env_base if exists.
+def load_izienv(name: str, path_envs: Path = Path(".envs"), override: bool = True) -> None:
+    path_env = path_env(path_envs=path_envs, name=name)
+    if not path_env.exists():
+        raise ValueError(f"{path_env} dont exists.")
+    
+    # Load `base.env` if exists.
     __load_env_base(path_envs=path_envs, override=override)
 
-    # Load .env_{name}.
-    path_env = __path_env(path_envs=path_envs, name=name)
+    # Load `{name}.env`.
     load_dotenv(path_env, override=override)
+
+    # Load `etc.env` if exists.
+    __load_env_etc(path_envs=path_envs, override=override)
