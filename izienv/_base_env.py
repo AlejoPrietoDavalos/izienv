a7d5c5960a86d036ceedb8c3f579f3d62691c5ea
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-03 16:29:45 -0300

    0.0.7 -> 0.0.8, load_env_var for str, path, int and float

--- izienv/_base_env.py
+++ izienv/_base_env.py
@@ -1,4 +1,4 @@
-from typing import Type, TypeVar, Callable
+from typing import Type, TypeVar, Callable, Any
 from pathlib import Path
 import os
 
@@ -16,20 +16,23 @@ class BaseEnv:
     def name_upper(self) -> str:
         return self.name.upper()
 
+
 def load_env_var(
         *,
         name_pre: bool = False,
-        is_path: bool = False,
-        raise_none_error: bool = True
+        raise_none_error: bool = True,
+        cast_to: Type[Any]
     ):
     """
+    Generic decorator to load environment variables and cast to a specific type.
+    
     - `name_pre (bool):` Add the `upper_name` to the left of the variable. Separated by "_".
-    - `is_path: (bool):` Returns the path instead of the string.
     - `raise_none_error (bool):` Raise error if the `env_var` is None when load.
+    - `cast_to (Type[Any]):` Type to cast the env var to a specific type.
     """
     def decorator(func: Callable[[Type[T_BaseEnv]], str]):
         """ Raise error if result is None."""
-        def wrapper(self: Type[T_BaseEnv]) -> str | Path:
+        def wrapper(self: Type[T_BaseEnv]) -> Any:
             env_name = func(self)
             if not isinstance(env_name, str):
                 raise ValueError("Expected string for `env_name`.")
@@ -41,8 +44,38 @@ def load_env_var(
             if raise_none_error and env_var is None:
                 raise ValueError(f"`{func.__name__}` for `{self.name_upper}` is None")
             
-            if is_path:
-                return Path(env_var)
-            return env_var
+            return cast_to(env_var)
         return wrapper
     return decorator
+
+def load_env_var_str(
+        *,
+        name_pre: bool = False,
+        raise_none_error: bool = True
+    ):
+    """ Specific decorator for `str`. See `load_env_var` doc for understanding."""
+    return load_env_var(name_pre=name_pre, raise_none_error=raise_none_error, cast_to=str)
+
+def load_env_var_path(
+        *,
+        name_pre: bool = False,
+        raise_none_error: bool = True
+    ):
+    """ Specific decorator for `path`. See `load_env_var` doc for understanding."""
+    return load_env_var(name_pre=name_pre, raise_none_error=raise_none_error, cast_to=Path)
+
+def load_env_var_float(
+        *,
+        name_pre: bool = False,
+        raise_none_error: bool = True
+    ):
+    """ Specific decorator for `float`. See `load_env_var` doc for understanding."""
+    return load_env_var(name_pre=name_pre, raise_none_error=raise_none_error, cast_to=float)
+
+def load_env_var_int(
+        *,
+        name_pre: bool = False,
+        raise_none_error: bool = True
+    ):
+    """ Specific decorator for `int`. See `load_env_var` doc for understanding."""
+    return load_env_var(name_pre=name_pre, raise_none_error=raise_none_error, cast_to=int)
