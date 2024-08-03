54513b0fb26c0ffbb69fd7e5f3d6ec372218c69e
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-03 16:08:56 -0300

    Path for env_var

--- izienv/_base_env.py
+++ izienv/_base_env.py
@@ -1,4 +1,5 @@
 from typing import Type, TypeVar, Callable
+from pathlib import Path
 import os
 
 T_BaseEnv = TypeVar("T_BaseEnv", bound="BaseEnv")
@@ -15,14 +16,20 @@ class BaseEnv:
     def name_upper(self) -> str:
         return self.name.upper()
 
-def load_env_var(*, name_pre: bool = False, raise_none_error: bool = True):
+def load_env_var(
+        *,
+        name_pre: bool = False,
+        is_path: bool = False,
+        raise_none_error: bool = True
+    ):
     """
     - `name_pre (bool):` Add the `upper_name` to the left of the variable. Separated by "_".
+    - `is_path: (bool):` Returns the path instead of the string.
     - `raise_none_error (bool):` Raise error if the `env_var` is None when load.
     """
     def decorator(func: Callable[[Type[T_BaseEnv]], str]):
         """ Raise error if result is None."""
-        def wrapper(self: Type[T_BaseEnv]) -> str:
+        def wrapper(self: Type[T_BaseEnv]) -> str | Path:
             env_name = func(self)
             if not isinstance(env_name, str):
                 raise ValueError("Expected string for `env_name`.")
@@ -33,6 +40,9 @@ def load_env_var(*, name_pre: bool = False, raise_none_error: bool = True):
             env_var = os.getenv(env_name)
             if raise_none_error and env_var is None:
                 raise ValueError(f"`{func.__name__}` for `{self.name_upper}` is None")
+            
+            if is_path:
+                return Path(env_var)
             return env_var
         return wrapper
     return decorator
